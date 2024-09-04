bcd4d8de9934d5ad51e153cce8e2c36a71d9b21e
dewapurnama <75442383+dewapurnama@users.noreply.github.com>
2024-09-04 10:22:00 +0700

    Update cleansing_brj_spm.py

--- cleansing_brj_spm.py
+++ cleansing_brj_spm.py
@@ -17,7 +17,7 @@ brj_file = st.file_uploader("Upload File BRJ disini", type=['xls','xlsx'])
 buffer = io.BytesIO()
 if brj_file is not None:
     brj = pd.read_excel(brj_file)
-    st.dataframe(brj.head(100))
+    st.dataframe(brj)
 #df_brj = pd.read_excel('C:/Users/User/Documents/ODP BPKH/OJT/A2/Cleansing BTAW/ibbankreportjournal_2022.xlsx')
 
 
