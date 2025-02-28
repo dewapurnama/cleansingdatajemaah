75e971d5b0ff52170c3e23fb0d3fbd9ea8b072ef
dewapurnama <75442383+dewapurnama@users.noreply.github.com>
2025-02-28 10:21:32 +0700

    Update cleansing_brj_spm.py

--- cleansing_brj_spm.py
+++ cleansing_brj_spm.py
@@ -303,9 +303,9 @@ elif option == "Setoral Awal":
         st.dataframe(df_skh.head(100))
 
         # Apply the function to the columns
-        df_skh.loc[:, 'portion'] = df_spm['portion'].apply(lambda x: f'0{x}' if len(str(x)) == 9 else str(x))
-        df_skh.loc[:, 'validation'] = df_spm['validation'].apply(modify_value)
-        df_spm.loc[:, 'portion'] = df_spm['portion'].apply(modify_value)
+        df_skh.loc[:, 'portion'] = df_skh['portion'].apply(lambda x: f'0{x}' if len(str(x)) == 9 else str(x))
+        df_skh.loc[:, 'validation'] = df_skh['validation'].apply(modify_value)
+        df_spm.loc[:, 'portion'] = df_skh['portion'].apply(modify_value)
 
         # Calculate the sum of nilai_mutasi for C and D
         grouped = filtered_df_brj.groupby('parsing_deskripsi', as_index=False, group_keys=False).apply(
